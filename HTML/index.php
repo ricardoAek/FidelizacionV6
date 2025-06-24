a49729214a6267bd39e4df8d922c24d9b5a3ef38
ricardoek610 <ricardoek610@gmail.com>
2025-06-24 11:39:31 -0600

    Historia

--- HTML/index.php
+++ HTML/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Mustang - Home</title>
+    <title>Mustang -- Home</title>
     <?php include '../Layout/documentCDN.html'; ?>
 </head>
 <body>
@@ -19,9 +19,10 @@
 
             <!-- Sección de ventajas -->
             <div class="mt-4">
-                <h3></h3>
+                <h3>La historia del Mustang Shelby es la unión del muscle car Ford Mustang con la visión de alto rendimiento de Carroll Shelby. Desde 1965, Shelby American transformó Mustangs en máquinas de carreras, comenzando con el GT350, y la colaboración continuó con modelos como el GT500, conocidos por su potencia y presencia en pista. </h3>
                 <ul>
                     <li
+                    
                     li>
                    
                 </ul>                
